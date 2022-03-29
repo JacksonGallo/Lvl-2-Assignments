bd9022539b071f8c8b5332d06447b5b17da9d816
Jackson Gallo <gallo.60@icloud.com>
2022-03-29 17:28:25 -0400

    mario pest complete

--- mario-pest-control/main.js
+++ mario-pest-control/main.js
@@ -1,3 +1,15 @@
-const goombaForm = document.form.goombaForm
-const bobombForm = document.form.bobombForm
-const cheepForm = document.form.cheepForm
+const baddieForm = document.baddieForm
+
+baddieForm.addEventListener("submit", (event) => {
+    event.preventDefault()
+
+    const goombaCount = parseInt(baddieForm.goomba.value,10)
+    const bobombCount = parseInt(baddieForm.bobomb.value,10)
+    const cheepCount = parseInt(baddieForm.cheep.value,10)
+
+    const totalPrice = document.createElement("h2")
+    totalPrice.textContent=((goombaCount*5)+(bobombCount*7)+(cheepCount*11))
+    document.getElementById("total").append(totalPrice)
+
+    
+})
