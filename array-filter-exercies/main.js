a9b6597ccfdef9e1c9776c1e9217bae55595596f
Jackson Gallo <gallo.60@icloud.com>
2022-03-30 11:20:54 -0400

    afe complete

---
+++ array-filter-exercies/main.js
@@ -0,0 +1,54 @@
+//return new array of numbers > 5
+//const arr = [1,2,3,4,5,6,7,8]
+
+//const greaterThan = arr.filter(num => num > 5)
+
+//console.log(greaterThan)
+
+
+//return only even numbers
+//const arr = [1,2,3,4,5,6,7,8]
+
+//const onlyEven = arr.filter(num => num % 2 === 0)
+
+//console.log(onlyEven)
+
+
+//given array of strings, return only those that are <= 5 char in length
+
+//const arr = ["dog", "wolf", "by", "family", "eaten", "camping"]
+
+//const fiveOrFewer = arr.filter(word => word.length <=5)
+
+//console.log(fiveOrFewer)
+
+
+
+//return only members
+/*
+const arr = [
+    { name: "Angelina Jolie", member: true },
+    { name: "Eric Jones", member: false },
+    { name: "Paris Hilton", member: true },
+    { name: "Kayne West", member: false },
+    { name: "Bob Ziroll", member: true }
+]
+
+const members = arr.filter(member => member.member === true)
+
+console.log(members)
+*/
+
+
+//people old enough to see the Matrix
+const arr = [
+    { name: "Angelina Jolie", age: 80 },
+    { name: "Eric Jones", age: 2 },
+    { name: "Paris Hilton", age: 5 },
+    { name: "Kayne West", age: 16 },
+    { name: "Bob Ziroll", age: 100 }
+]
+
+const oldEnough = arr.filter(person => person.age >= 18)
+
+console.log(oldEnough)
