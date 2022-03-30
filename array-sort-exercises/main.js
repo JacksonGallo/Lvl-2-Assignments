823ff9b2948712ef5f0342b61ca7d78943b9c2ed
Jackson Gallo <gallo.60@icloud.com>
2022-03-30 16:58:47 -0400

    ase complete

---
+++ array-sort-exercises/main.js
@@ -0,0 +1,46 @@
+//code challenge
+//const total = (num1, num2) => (num1*2)+(num2*3)
+//console.log(total(7,5))
+
+//sort from smallest to largest
+
+//const arr = [1, 3, 5, 2, 90, 20]
+
+//arr.sort((a,b) => a - b)
+//arr.sort((a,b) => b - a)
+
+//console.log(arr)
+
+//sort alphabetically
+
+//const arr = ["dog", "wolf", "by", "family", "eaten"]
+
+//arr.sort((a,b) => a === b ? 0 : a < b ? -1 : 1)
+
+//console.log(arr)
+
+
+//sort from shortest string to longest
+
+//const arr = ["dog", "wolf", "by", "family", "eaten"]
+
+//arr.sort((a,b) => a.length - b.length)
+
+//console.log(arr)
+
+
+//sort objects in the array by age
+
+/*
+const arr = [
+    { name: "Quiet Samurai", age: 22 },
+    { name: "Arrogant Ambassador", age: 100 },
+    { name: "Misunderstood Observer", age: 2 },
+    { name: "Unlucky Swami", age: 77 }
+]
+
+arr.sort((a,b) => a.age - b.age)
+
+console.log(arr)
+
+*/
