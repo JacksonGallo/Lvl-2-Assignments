a691a11fa77379db9f65dd8ca94f712ea7dd73a6
Jackson Gallo <gallo.60@icloud.com>
2022-04-27 09:45:58 -0400

    initial

--- 3-level/random-color/src/App.js
+++ 3-level/random-color/src/App.js
@@ -1,21 +1,9 @@
 import React from "react"
 import axios from "axios"
-// const axios = require ("axios")
+
 export default function App(){
 
   const [color, setColor] = React.useState("")
-
-  // const newColor = () => {
-  //   axios.get(`https://www.colr.org/json/color/random?timestamp=${new Date().getTime()}`)
-  // // .then(data => console.log(data))
-  // .then(res => {
-  //   setColor(prevColor => [...prevColor], res.data.colors[0].hex)
-  //   }
-  // )}
-
-  // React.useEffect(() => {
-  //   newColor()
-  // }, [])
 
   const randomNum = Math.floor(Math.random())
 
